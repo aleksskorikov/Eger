6785df89343405b834c4acb8487366d2b31ebeb0
aleksskorikov <aleksandrskorikov123@gmail.com>
2023-11-28 17:48:25 +0200

    add button on all

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,43 @@ $(document).ready(function () {
 		slidesToShow: 3,
 		// centerMode: true,
 	});
-});
+})
+
+
+// кнопка на верх
+document.getElementById("on-all").onclick = function ()  {
+document.getElementById("header").scrollIntoView({behavior:"smooth"});
+}
+
+// меню на главной странице
+
+let links = document.querySelectorAll(".nav__buttom");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior:"smooth"});
+    }
+}
+
+// форма емфил
+
+const btns = document.querySelectorAll(".btn");
+const overley = document.querySelector(".wrapper");
+const form = document.querySelector(".form");
+const body = document.querySelector("body");
+const cloze = document.querySelector(".cloze")
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        overley.style.display = "flex"
+        body.style.overflow = "hidden"
+    })
+})
+
+form.addEventListener("click", function (e) {
+    e.stopPropagation();
+})
+
+cloze.addEventListener("click", function () {
+    overley.style.display = "none"
+    body.style.overflow = "auto"
+})
